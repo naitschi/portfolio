0831f73b54e429633379e022d083c30368f02919
ses <ses@e-fork.de>
2023-02-03 16:10:42 +0100

    added Navbar

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import NavbarPortfolio from '../components/Navbar';
+import { ChakraProvider } from "@chakra-ui/react";
+
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <ChakraProvider>
+        <body>
+          <NavbarPortfolio />
+          <Main />
+          <NextScript />
+        </body>
+      </ChakraProvider>
     </Html>
-  )
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Flex, Heading } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Portfolio | About Me</title>
-        <meta name="description" content="Portfolio | About Me " />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>About Sebastian Schieck</h1>
-      </main>
-    </>
-  )
+    <Flex height='100vh' alignItems='center' justifyContent={"center"}>
+      <Flex direction='column' background='gray.100' p='12' rounded={6}>
+        <Heading mb={6}>Login</Heading>
+      </Flex>
+    </Flex>
+  );
 }
