66a4fdb80ab4f3a716198c3d632524cdcaa4602a
ses <sebastian.schieck91@gmail.com>
2023-02-05 12:06:14 +0100

    start working on layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ export default function NavbarPortfolio() {
             onClick={isOpen ? onClose : onOpen}
           />
           <HStack spacing={8} alignItems={"center"}>
-            <Box>Logo</Box>
+            <Box>SES</Box>
             <HStack
               as={"nav"}
               spacing={4}
@@ -90,12 +90,7 @@ export default function NavbarPortfolio() {
                 cursor={"pointer"}
                 minW={0}
               >
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
+                <Avatar size={"sm"} src={"/public/images/1102269.jpg"} />
               </MenuButton>
               <MenuList>
                 <MenuItem>Link 1</MenuItem>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
+import NavbarPortfolio from "@/components/Navbar";
 import { Flex, Heading } from "@chakra-ui/react";
-import Head from "next/head";
 
 export default function Home() {
   return (
-    <Flex height='100vh' alignItems='center' justifyContent={"center"}>
-      <Flex direction='column' background='gray.100' p='12' rounded={6}>
-        <Heading mb={6}>Login</Heading>
+    <main>
+      <NavbarPortfolio />
+      <Flex height='100vh' alignItems='center' justifyContent={"center"}>
+        <Flex direction='column' background='gray.100' p='12' rounded={6}>
+          <Heading mb={6}>Login</Heading>
+        </Flex>
       </Flex>
-    </Flex>
+    </main>
   );
 }
