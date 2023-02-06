a76cd9a412b5cdb5edac6decc89800e5298ee137
ses <ses@e-fork.de>
2023-02-06 10:03:18 +0100

    added intro section

---
+++ src/components/intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Heading, Box } from "@chakra-ui/react";
+
+const Intro = (props) => {
+  return (
+    <Box boxSize={"sm"}>
+      <Heading>Intro</Heading>
+    </Box>
+  );
+};
+
+Intro.propTypes = {};
+
+export default Intro;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import FooterSes from "./footer";
+import Intro from "./intro";
 import NavbarPortfolio from "./Navbar";
 
 export const Layout = (props) => {
@@ -9,6 +10,7 @@ export const Layout = (props) => {
   return (
     <div>
       <NavbarPortfolio />
+      <Intro />
       <Box as='main'>{children}</Box>
       <FooterSes />
     </div>
