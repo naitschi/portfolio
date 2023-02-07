7ffe83d0f79317b93c3be2da5bf3b5c66dbc6c59
ses <ses@e-fork.de>
2023-02-07 15:46:15 +0100

    content changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,31 @@
-import { ReactNode } from "react";
+import React from "react";
 import {
   Box,
   Flex,
-  Avatar,
   HStack,
   Link,
   IconButton,
   Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
   useColorMode,
   Stack,
   Image,
 } from "@chakra-ui/react";
-import {
-  HamburgerIcon,
-  CloseIcon,
-  AddIcon,
-  MoonIcon,
-  SunIcon,
-} from "@chakra-ui/icons";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 
-const Links = ["Dashboard", "Projects", "Team"];
+const Links = [
+  {
+    linkText: "Who am I?",
+    href: "/",
+  },
+  {
+    linkText: "Work",
+    href: "/about",
+  },
+];
 
-const NavLink = ({ children }) => (
+const NavLink = ({ item }) => (
   <Link
     px={2}
     py={1}
@@ -37,9 +34,9 @@ const NavLink = ({ children }) => (
       textDecoration: "none",
       bg: useColorModeValue("gray.200", "gray.700"),
     }}
-    href={"#"}
+    href={item.href}
   >
-    {children}
+    {item.linkText}
   </Link>
 );
 
@@ -60,10 +57,9 @@ export default function NavbarPortfolio() {
           />
           <HStack spacing={8} alignItems={"center"}>
             <Image
-              boxSize='64px'
-              rounded={6}
-              alt='SES Development'
-              src='/Images/ses_development.png'
+              boxSize="64px"
+              alt="SES Development"
+              src="/Images/ses_development.png"
             />
             <HStack
               as={"nav"}
@@ -71,40 +67,14 @@ export default function NavbarPortfolio() {
               display={{ base: "none", md: "flex" }}
             >
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <NavLink item={link} key={link} />
               ))}
             </HStack>
           </HStack>
           <Flex alignItems={"center"}>
-            <Button
-              variant={"solid"}
-              colorScheme={"teal"}
-              size={"sm"}
-              mr={4}
-              leftIcon={<AddIcon />}
-            >
-              Action
-            </Button>
             <Button mr={4} onClick={toggleColorMode}>
               {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
             </Button>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={"full"}
-                variant={"link"}
-                cursor={"pointer"}
-                minW={0}
-              >
-                <Avatar size={"sm"} src={"/public/images/1102269.jpg"} />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu>
           </Flex>
         </Flex>
 
@@ -112,7 +82,7 @@ export default function NavbarPortfolio() {
           <Box pb={4} display={{ md: "none" }}>
             <Stack as={"nav"} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <NavLink item={link} key={link} />
               ))}
             </Stack>
           </Box>
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Heading, Box } from "@chakra-ui/react";
+import {
+  Heading,
+  Box,
+  Grid,
+  GridItem,
+  Flex,
+  ButtonGroup,
+  Spacer,
+  Button,
+} from "@chakra-ui/react";
 
 const Intro = (props) => {
   return (
-    <Box boxSize={"sm"}>
-      <Heading>Intro</Heading>
-    </Box>
+    <Grid gap={4} templateColumns="repeat(12, 1fr)">
+      <GridItem colSpan={12}>
+        <Flex
+          minWidth="max-content"
+          justifyContent={"flex-start"}
+          alignItems="center"
+        >
+          <Box p="12">
+            <Heading size="md">Who am I?</Heading>
+          </Box>
+        </Flex>
+      </GridItem>
+    </Grid>
   );
 };
 
