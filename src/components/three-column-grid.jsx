cb196d1e42336b7d6b8e09b64ca4af9069f94bf2
ses <ses@e-fork.de>
2023-02-12 14:21:43 +0100

    icon finework

--- src/components/three-column-grid.jsx
+++ src/components/three-column-grid.jsx
@@ -13,16 +13,7 @@ import { FcAssistant, FcDonate, FcInTransit } from "react-icons/fc";
 const Feature = ({ title, text, icon }) => {
   return (
     <Stack>
-      <Flex
-        w={"full"}
-        h={16}
-        align={"center"}
-        justify={"center"}
-        color={"white"}
-        rounded={"full"}
-        bg={"gray.100"}
-        mb={1}
-      >
+      <Flex w={"full"} align={"center"} justify={"center"}>
         {icon}
       </Flex>
       <Flex w={"full"} align={"center"} justify={"center"}>
@@ -39,21 +30,60 @@ export default function SimpleThreeColumns() {
       <Container maxW={"6xl"} mt={10}>
         <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
           <Feature
-            icon={<Icon as={FcAssistant} w={10} h={10} />}
+            icon={
+              <Flex
+                w={16}
+                h={16}
+                align={"center"}
+                justify={"center"}
+                color={"white"}
+                rounded={"full"}
+                bg={"gray.100"}
+                mb={1}
+              >
+                <Icon as={FcAssistant} w={10} h={10} />
+              </Flex>
+            }
             title={"Lifetime Support"}
             text={
               "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore..."
             }
           />
           <Feature
-            icon={<Icon as={FcDonate} w={10} h={10} />}
+            icon={
+              <Flex
+                w={16}
+                h={16}
+                align={"center"}
+                justify={"center"}
+                color={"white"}
+                rounded={"full"}
+                bg={"gray.100"}
+                mb={1}
+              >
+                <Icon as={FcDonate} w={10} h={10} />
+              </Flex>
+            }
             title={"Unlimited Donations"}
             text={
               "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore..."
             }
           />
           <Feature
-            icon={<Icon as={FcInTransit} w={10} h={10} />}
+            icon={
+              <Flex
+                w={16}
+                h={16}
+                align={"center"}
+                justify={"center"}
+                color={"white"}
+                rounded={"full"}
+                bg={"gray.100"}
+                mb={1}
+              >
+                <Icon as={FcInTransit} w={10} h={10} />
+              </Flex>
+            }
             title={"Instant Delivery"}
             text={
               "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore..."
